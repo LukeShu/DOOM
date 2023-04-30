ca76d5ca3660e0941affa680f14cfa7f01643744
Luke Shumaker <lukeshu@lukeshu.com>
2023-04-30 06:15:14 -0600

    scaling

--- doomgeneric/doomgeneric_aalib.c
+++ doomgeneric/doomgeneric_aalib.c
@@ -4,11 +4,12 @@
 
 #include <ctype.h>  /* for tolower(3p) */
 #include <error.h>  /* for error(3gnu) */
+#include <stdio.h>  /* for puts(3p) */
 #include <stdlib.h> /* for setenv(3p) and unsetenv(3p) */
 
 static aa_context *context = NULL;
 
-#define min(a, b) ((a) < (b) ? (a) : (b))
+#define div_roundup(n, d) ((n + d - 1) / d)
 
 int main(int argc, char **argv) {
 	if (!aa_parseoptions(NULL, NULL, &argc, argv)) {
@@ -42,19 +43,28 @@ void DG_DrawFrame() {
 	}
 
 	unsigned char *out_buffer = aa_image(context);
-	int out_resx = aa_imgwidth(context);
-	int out_resy = aa_imgheight(context);
+	int doomx_per_outx = div_roundup(DOOMGENERIC_RESX, aa_imgwidth(context));
+	int doomy_per_outy = div_roundup(DOOMGENERIC_RESY, aa_imgheight(context)-1);
+	int out_resx = DOOMGENERIC_RESX / doomx_per_outx;
+	int out_resy = DOOMGENERIC_RESY / doomy_per_outy;
+	int full_out_resx = aa_imgwidth(context);
 
 	//aa_close(context);
 	//error(1, 0, "resx=%d resy=%d", out_resx, out_resy);
 
-	uint32_t r, g, b;
-	for (unsigned int y = 0; y < min(DOOMGENERIC_RESY, out_resy); y++) {
-		for (unsigned int x = 0; x < min(DOOMGENERIC_RESX, out_resx); x++) {
-			r = (DG_ScreenBuffer[y*DOOMGENERIC_RESX+x] >> 24) & 0xFF;
-			g = (DG_ScreenBuffer[y*DOOMGENERIC_RESX+x] >> 16) & 0xFF;
-			b = (DG_ScreenBuffer[y*DOOMGENERIC_RESX+x] >>  8) & 0xFF;
-			out_buffer[y*out_resx+x] = (r + g + b) / 3;
+	uint32_t v, r, g, b;
+	for (int oy = 0; oy < out_resy; oy++) {
+		for (int ox = 0; ox < out_resx; ox++) {
+			v = 0;
+			for (int dy = oy*doomy_per_outy; dy < (oy+1)*doomy_per_outy; dy++) {
+				for (int dx = ox*doomx_per_outx; dx < (ox+1)*doomx_per_outx; dx++) {
+					r = (DG_ScreenBuffer[dy*DOOMGENERIC_RESX+dx] >> 24) & 0xFF;
+					g = (DG_ScreenBuffer[dy*DOOMGENERIC_RESX+dx] >> 16) & 0xFF;
+					b = (DG_ScreenBuffer[dy*DOOMGENERIC_RESX+dx] >>  8) & 0xFF;
+					v += (r + g + b) / 3;
+				}
+			}
+			out_buffer[oy*full_out_resx+ox] = v/(doomx_per_outx*doomy_per_outy);
 		}
 	}
 
