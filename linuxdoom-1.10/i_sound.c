bd488e70265c0164c461cf5e6918faac24acb9ae
Luke Shumaker <lukeshu@lukeshu.com>
2023-05-15 15:22:12 -0600

    Get it to compile on modern x86-32 GNU/Linux

--- linuxdoom-1.10/i_sound.c
+++ linuxdoom-1.10/i_sound.c
@@ -30,6 +30,7 @@ rcsid[] = "$Id: i_unix.c,v 1.5 1997/02/03 22:45:10 b1 Exp $";
 #include <stdarg.h>
 
 #include <math.h>
+#include <errno.h>
 
 #include <sys/time.h>
 #include <sys/types.h>
@@ -164,7 +165,6 @@ myioctl
   int*	arg )
 {
     int		rc;
-    extern int	errno;
 
     rc = ioctl(fd, command, arg);
     if (rc < 0)
